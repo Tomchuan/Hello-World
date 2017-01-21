f235690af6c1ca414be98462597507505dcf8604
Tomchuan <shengchuan03@163.com>
2017-01-22 00:06:13 +0800

    print "Hello-World"

---
+++ src/org/function/java/Test_01.java
@@ -0,0 +1,11 @@
+package org.function.java;
+
+public class Test_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello-World");
+
+	}
+
+}
